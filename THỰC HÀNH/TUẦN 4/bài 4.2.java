be973e1ca09cf4be982418e838af8ed28c503872
NguyenthutrangGd24A110735 <trang110735@donga.edu.vn>
2024-11-03 19:54:48 +0700

    Add files via upload

---
+++ THỰC HÀNH/TUẦN 4/bài 4.2.java
@@ -0,0 +1,7 @@
+public static void bai4_2() {
+    String nguHanh[] = { "kim", "mộc", "thủy", "hỏa", "thổ" };
+    System.out.print("\nDanh sách phần tử của mảng nguHanh là: ");
+    for (int i = 0; i < nguHanh.length; i++) {
+        System.out.printf("\nnguHanh[%d] = %s", i, nguHanh[i]);
+    }
+}
